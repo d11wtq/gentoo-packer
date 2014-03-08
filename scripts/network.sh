c7414038bcfeee9620e83b921dea628329fbe626
d11wtq <chris@w3style.co.uk>
2014-03-08 17:02:04 +1100

    Make sure /etc/conf.d/net exists

--- scripts/network.sh
+++ scripts/network.sh
@@ -3,5 +3,6 @@
 chroot /mnt/gentoo /bin/bash <<'EOF'
 touch /etc/udev/rules.d/80-net-name-slot.rules
 ln -s /etc/init.d/net.lo /etc/init.d/net.eth0
+echo 'config_eth0=( "dhcp" )' >> /etc/conf.d/net
 rc-update add net.eth0 default
 EOF
