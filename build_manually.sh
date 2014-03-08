20c02f6050133b41c51b92de74484897c20c60e6
d11wtq <chris@w3style.co.uk>
2014-03-09 06:23:49 +1100

    Add script to manually run from inside a Gentoo box

---
+++ build_manually.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [[ -z $STAGE3 ]]
+then
+  echo "STAGE3 environment variable must be set to a timestamp."
+  exit 1
+fi
+
+for script in \
+  partition   \
+  stage3      \
+  mounts      \
+  portage     \
+  timezone    \
+  fstab       \
+  kernel      \
+  grub        \
+  virtualbox  \
+  network     \
+  vagrant     \
+  cleanup
+do
+  path="./scripts/$script.sh"
+  echo "Start $path"
+  "$path"
+  echo "Finished $path"
+done
+
+echo "All done, shutting down in"
+for s in 5 4 3 2 1
+do
+  echo -n " $s"
+done
+
+shutdown -hP now
