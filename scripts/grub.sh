6f31c0ce302804ae39f17e7e461d83bc188120a3
d11wtq <chris@w3style.co.uk>
2014-03-10 00:31:27 +1100

    Install grub before running mkconfig

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -5,6 +5,6 @@ emerge ">=sys-boot/grub-2.0"
 sed -i 's/^#GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="net.ifnames=0"/' \
   /etc/default/grub
 echo "set timeout=0" >> /etc/grub.d/40_custom
-grub2-mkconfig -o /boot/grub/grub.cfg
 grub2-install /dev/sda
+grub2-mkconfig -o /boot/grub/grub.cfg
 EOF
