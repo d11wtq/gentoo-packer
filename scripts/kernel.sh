30a21a7f839d60c9e4d62de7007295fc46cc2e84
d11wtq <chris@w3style.co.uk>
2014-03-10 14:35:33 +1100

    Do use genkernel (for the initramfs), but remove it once done

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -2,10 +2,9 @@
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
 emerge sys-kernel/gentoo-sources
+emerge sys-kernel/genkernel
 cd /usr/src/linux
 make defconfig
-make -j2
-make modules_install
-cp arch/x86_64/boot/bzImage /boot/kernel
-cp System.map /boot/System.map
+genkernel --install --symlink --oldconfig all
+emerge -c sys-kernel/genkernel
 EOF
