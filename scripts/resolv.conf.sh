7c7efe38a3674d1e57fc79cc46eb482adea207bf
Michael R. Lawrence <mike@michaellawrenceit.com>
2015-11-17 20:25:04 -0600

    Update resolv.conf.sh

--- scripts/resolv.conf.sh
+++ scripts/resolv.conf.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
-cp -L /etc/resolv.conf /mnt/gentoo/etc/
+# Asumption ATLUS PACKER is doing Automatic builds with 0 human's at the helm 
+# should the first Of the machines DNS FAIL or not match new end users machine on boot /DHCP less fuss.
+cat >>/mnt/gentoo/etc/resolv.conf <<EOF
+nameserver 8.8.8.8  #Google Primary Public DNS
+nameserver 8.8.4.4  #Google Secoundary Public DNS
+nameserver 2001:4860:4860::8888 #Google Primary Public DNS IPV6
+nameserver 2001:4860:4860::8844 #Google Secoundary Public DNS IPV6
+EOF
