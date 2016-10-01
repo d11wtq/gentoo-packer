64cc433087e6dcf793c88a8831a474f641e4b5f6
Jonathan Alainis <jonathan@alainis.de>
2016-10-01 17:42:26 +0200

    Update virtualbox-guest-additions to 5.0.20

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -5,9 +5,9 @@ emerge "=virtual/linux-sources-1" --autounmask-write
 etc-update --automode -5
 emerge "=virtual/linux-sources-1"
 
-emerge ">=app-emulation/virtualbox-guest-additions-4.3" --autounmask-write
+emerge ">=app-emulation/virtualbox-guest-additions-5.0.20" --autounmask-write
 etc-update --automode -5
-emerge ">=app-emulation/virtualbox-guest-additions-4.3"
+emerge ">=app-emulation/virtualbox-guest-additions-5.0.20"
 
 rc-update add virtualbox-guest-additions default
 EOF
